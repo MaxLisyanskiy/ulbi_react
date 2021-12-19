7f02ba53c90d34b05f4d864db3f6cc3aab2e8575
Максим Лисянский <max_lisyanskiy@mail.ru>
2021-12-20 00:41:33 +0300

    finished sort function

--- src/App.js
+++ src/App.js
@@ -2,27 +2,50 @@ import React, {useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'JS', body: 'Descr'},
-        {id: 2, title: 'JS 2', body: 'Descr'},
-        {id: 3, title: 'JS 3', body: 'Descr'},
-        {id: 4, title: 'JS 4', body: 'Descr'},
-        {id: 5, title: 'JS 5', body: 'Descr'},
+        {id: 1, title: 'rrrrrr', body: 'vvvvvvv'},
+        {id: 2, title: 'hhhhhh', body: 'aaaaa'},
+        {id: 3, title: 'asdas', body: 'eeeeeee'},
+        {id: 4, title: 'xcxzx', body: 'wwwww'},
+        {id: 5, title: 'qqqqqq', body: 'qqqq'},
     ])
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
+    const removePost = (post) => {
+       setPosts(posts.filter(p => p.id !== post.id ))
+    }
+
+    const sortPosts = (sort) => {
+      setSelectedSort(sort);
+      setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList
-                posts={posts}
-                title={'Список постов'}
-            />
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+              <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Сортировка по:"
+                options={[
+                  {value: 'title', name: 'По заголовку'},
+                  {value: 'body', name: 'По описанию'}
+                ]}
+              />
+            </div>
+            {posts.length !== 0 
+              ? <PostList posts={posts} title={'Список постов'} remove={removePost}/>
+              : <div>Посты не найдены!</div>
+            }
         </div>
     );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -9,7 +9,7 @@ const PostItem = (props) => {
                     <div>{props.post.body}</div>
                 </div>
                 <div className="post__btns">
-                    <button>Удалить</button>
+                    <button onClick={() => props.remove(props.post)}>Удалить</button>
                 </div>
             </div>
         </div>
