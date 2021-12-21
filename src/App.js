d4d00d7c2a742abe486241065a3d99b691b29124
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2021-12-21 21:48:29 +0300

    added new hook and modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,23 +15,13 @@ function App() {
         {id: 4, title: 'xcxzx', body: 'wwwww'},
         {id: 5, title: 'qqqqqq', body: 'qqqq'},
     ])
-    const[filter, setFilter] = useState({sort: '', query: ''})
-
-    // сортировка
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts])
-
-    // поиск
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const createPost = (newPost) => {
-        setPosts([...posts, newPost])
+        setPosts([...posts, newPost]);
+        setModal(false)
     }
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -36,13 +29,15 @@ function App() {
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            {sortedAndSearchedPosts.length !== 0
-                ? <PostList posts={sortedAndSearchedPosts} title={'Список постов'} remove={removePost}/>
-                : <h2>Посты не найдены!</h2>
-            }
+            <PostList posts={sortedAndSearchedPosts} title={'Список постов'} remove={removePost}/>
         </div>
     );
 }
