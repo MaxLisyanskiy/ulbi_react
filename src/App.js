3746e4e50704a0d2a7eebb3094b0c57f4b377579
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2021-12-23 23:58:10 +0300

    paginations 23.12

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,18 +6,33 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import MyLoader from "./components/UI/loader/MyLoader";
+import {useFetching} from "./hooks/useFetching";
+import {getPageCount} from "./utils/pages";
+import {usePagesArray} from "./hooks/usePaginations";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'rrrrrr', body: 'vvvvvvv'},
-        {id: 2, title: 'hhhhhh', body: 'aaaaa'},
-        {id: 3, title: 'asdas', body: 'eeeeeee'},
-        {id: 4, title: 'xcxzx', body: 'wwwww'},
-        {id: 5, title: 'qqqqqq', body: 'qqqq'},
-    ])
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''});
-    const [modal, setModal] = useState(false)
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    let pagesArray = usePagesArray(totalPages);
+
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount, limit))
+    })
+
+    useEffect( () => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -25,6 +40,11 @@ function App() {
     }
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page);
+        fetchPosts();
     }
 
     return (
@@ -37,7 +57,25 @@ function App() {
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList posts={sortedAndSearchedPosts} title={'Список постов'} remove={removePost}/>
+            {postError && <h1>{postError}</h1>}
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <MyLoader/>
+                  </div>
+                : <PostList posts={sortedAndSearchedPosts} title={'Список постов'} remove={removePost}/>
+            }
+            <div className="page__wrapper">
+                {pagesArray.map( p =>
+                    <span
+                        key={p}
+                        className={page === p ? 'page page__current' : 'page'}
+                        onClick={() => changePage(p)}
+                    >
+                        {p}
+                    </span>
+                )}
+            </div>
+
         </div>
     );
 }
---
+++ src/components/UI/loader/MyLoader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './MyLoader.module.css'
+
+const MyLoader = () => {
+    return (
+        <div className={classes.roller}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default MyLoader;
